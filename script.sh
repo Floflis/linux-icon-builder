f256c16973df4ee7b00099908089293775d73c48
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-05-16 20:31:58 -0300

    Yes, it works 💞

--- script.sh
+++ script.sh
@@ -27,5 +27,21 @@ echo "Minimum dimensions: 512x512"
 echo -n "Please write the file path/name.extension:"
 read imgname
 if [ $(identify -ping -format '%w' $imgname) -ge 512 ]; then
-    echo "$(identify -ping -format '%w' $imgname)"
+    echo "Confirmed, it meets the minimum dimensions. File dimensions: $(identify -ping -format '%w' $imgname)"
+    echo -n "Please write the name you want for your icon:"
+    read applyname
+    convert $imgname    -resize 16x16  icons/16x16/$applyname
+    convert $imgname    -resize 32x32  icons/16x16@2x/$applyname
+    convert $imgname    -resize 32x32  icons/32x32/$applyname
+    convert $imgname    -resize 24x24  icons/24x24/$applyname
+    convert $imgname    -resize 48x48  icons/24x24@2x/$applyname
+    convert $imgname    -resize 48x48  icons/48x48/$applyname
+    convert $imgname    -resize 32x32  icons/32x32/$applyname
+    convert $imgname    -resize 64x64  icons/32x32@2x/$applyname
+    convert $imgname    -resize 96x96  icons/48x48@2x/$applyname
+    convert $imgname    -resize 256x256  icons/256x256/$applyname
+    convert $imgname    -resize 512x512  icons/256x256@2x/$applyname
+    echo "Done!"
+    else
+        echo "Please review how you've wrote/copied the file name. Usage example video: https://youtu.be/0fiKLsBVmEk"
 fi
